499bfec0bffc0c720a784fc3203d6ab1763d104c
Parthiban024 <114136939+Parthiban024@users.noreply.github.com>
2023-04-16 16:07:44 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from "./component/homePage"
+import Form from "./component/formPage"
+import FormTwo from "./component/formTimechamp"
+import FormThree from "./component/formHr"
+import Dashboard from "./component/dashboardPage"
+import DashboardTC from "./component/dashboardTC"
+import DashboardHR from "./component/dashboardHR"
+import LoginPage from "./component/loginPage"
+import LoginPageTwo from "./component/loginTC"
+import LoginPageThree from "./component/loginHR"
+import ReviewDetails from './component/viewPage'
+import ViewPageTwo from "./component/viewPageTC"
+import ViewPageThree from "./component/viewPageHR"
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route, Switch, BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/dashboard" exact element={<Dashboard />} />
+          <Route path="/form" exact element={<Form />} />
+          <Route path="/formTimechamp" exact element={<FormTwo />} />
+          <Route path="/formHr" exact element={<FormThree />} />
+          <Route path="/dashboardTC" exact element={<DashboardTC />} />
+          <Route path="/dashboardHR" exact element={<DashboardHR />} />
+          <Route path="/review/:id" element={<ReviewDetails />} />
+          <Route path="/reviewtwo/:id" element={<ViewPageTwo />} />
+          <Route path="/reviewthree/:id" element={<ViewPageThree />} />
+          <Route path="/login" exact element={<LoginPage />} />
+          <Route path="/loginTC" exact element={<LoginPageTwo />} />
+          <Route path="/loginHR" exact element={<LoginPageThree />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
